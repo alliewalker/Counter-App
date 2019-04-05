325622af445d47fefd136d053e7c8777af6ac5f4
alliewalker <walkera71193@gmail.com>
2019-04-05 09:34:22 -0400

    delete  works

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
-  };
-
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
@@ -13,57 +8,46 @@ class Counter extends Component {
 
   // experimental below using arrow function vs constructor()
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return null;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
   styles = {
     fontSize: 10,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    margin: 20,
+    padding: 10
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
+        {/* <h4>Counter{this.props.id}</h4> */}
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
-          Increment
+          + Increment
         </button>
-        {/* conditional rendering */}
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
-      </React.Fragment>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
     // We can use object destructoring so we dont have to use this.state each time
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
